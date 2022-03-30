3abefd137e2afd37246d0929ea7401a5a15fa87a
RaviSeta-at-237320554477 <ravi.seta@yobored.com>
2022-03-31 03:14:39 +0530

    minor change

--- TestCICD/ViewController.swift
+++ TestCICD/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: - view cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
